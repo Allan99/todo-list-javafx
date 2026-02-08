2e92dcf1e9cd624e9a79f5eaf5b0598b2303cbfc
Allan <allanpereira773@gmail.com>
2026-02-08 20:54:21 -0300

    Creates the todoItemDialog.fxml file

--- src/main/java/com/controlsfx/todolist/Controller.java
+++ src/main/java/com/controlsfx/todolist/Controller.java
@@ -5,11 +5,12 @@ import com.controlsfx.todolist.entities.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
@@ -27,38 +28,13 @@ public class Controller {
     @FXML
     private Label deadlineLabel;
 
+    @FXML
+    private BorderPane mainBorderPane;
+
     private List<TodoItem> todoItems;
 
 
     public void initialize(){
-//        TodoItem item1 = new TodoItem("Main birthday card",
-//                "Buy a 30th birthday card for John",
-//                LocalDate.of(2016, Month.APRIL, 25));
-//
-//        TodoItem item2 = new TodoItem("Doctor's appointment",
-//                "See Dr. Smith at 123 Main Street. Bring paperwork.",
-//                LocalDate.of(2016, Month.MAY, 23));
-//
-//        TodoItem item3 = new TodoItem("Finish design proposal for client",
-//                "I'd promised Mike I'd email website mockups by 22nd April.",
-//                LocalDate.of(2016, Month.APRIL, 22));
-//
-//        TodoItem item4 = new TodoItem("Pick up Doug at the train station.",
-//                "Doug is arriving on March 23 on the 5:00 train.",
-//                LocalDate.of(2016, Month.MARCH, 25));
-//
-//        TodoItem item5 = new TodoItem("Pick up dry cleaning",
-//                "The clothes should be ready by Wednesday.",
-//                LocalDate.of(2016, Month.APRIL, 20));
-//
-//        todoItems = new ArrayList<TodoItem>();
-//        todoItems.add(item1);
-//        todoItems.add(item2);
-//        todoItems.add(item3);
-//        todoItems.add(item4);
-//        todoItems.add(item5);
-//
-//        TodoData.getInstance().setTodoItems(todoItems);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -76,6 +52,21 @@ public class Controller {
         todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void showNewItemDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("todoItemDialog.fxml"));
+            dialog.getDialogPane().setContent(root);
+
+        }catch(Exception e){
+            System.out.println("Couldn't load the dialog.");
+            e.printStackTrace();
+            return;
+        }
     }
 
     @FXML
---
+++ src/main/java/com/controlsfx/todolist/DialogController.java
@@ -0,0 +1,4 @@
+package com.controlsfx.todolist;
+
+public class DialogController {
+}
